a00c82d7bdbc76be958d195fd561cb5506ec8e12
Islam Dudaev <islamwolf23@gmail.com>
2016-12-14 15:20:03 +0300

    adding errors to the form

--- register.php
+++ register.php
@@ -2,30 +2,71 @@
 // add the database connection script.
 include_once 'resource/db.php';
 
-if (isset($_POST['email'])){
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    //hashing the password
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+// process the form.
+if (isset($_POST['btn_register'])){
+    //initialize an array to store any error message from the form
+    $form_errors = array();
 
-    try {
-        // create SQL insert statement.
-        $sql = "INSERT INTO users (username, email, password, join_date)
+    //Form validation
+    $required_fields = array('email', 'username', 'password');
+
+    //loop through the required fields array and popular the form error array
+    foreach($required_fields as $field){
+        if(!isset($_POST[$field]) || $_POST[$field] == NULL){
+            $form_errors[] = $field." is a required field.";
+        }
+    }
+
+    //check if error array is empty, if yes process form data and insert record
+    if(empty($form_errors)) {
+        // collect form data and store in variables.
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // hashing the password.
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        try {
+            // create SQL insert statement.
+            $sql = "INSERT INTO users (username, email, password, join_date)
               VALUES (:username, :email, :password, now())";
 
-        // use PDO prepared to sanitize data.
-        $statement = $db->prepare($sql);
-        // add the data into the database.
-        $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+            // use PDO prepared to sanitize data.
+            $statement = $db->prepare($sql);
 
-        // check if one new row was created.
-        if($statement->rowCount() == 1) {
-            $result = "<p style='padding:20px; color: green;'> Registration Successful</p>";
+            // add the data into the database.
+            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+            // check if one new row was created.
+            if ($statement->rowCount() == 1) {
+                $result = "<p style='padding:20px; color: green;'> Registration Successful</p>";
+            }
+
+        } catch (PDOException $exception) {
+            $result = "<p style='padding:20px; color: red;'> An error occurred: " . $exception->getMessage() . "</p>";
         }
 
-    } catch (PDOException $exception){
-        $result = "<p style='padding:20px; color: red;'> An error occurred: ".$exception->getMessage()."</p>";
+    } else {
+        if(count($form_errors) == 1){
+            $result = "<p style='color: red;'> There was 1 error in the form:<br>";
+
+            $result .= "<ul style='color: red;'>";
+            //loop through error array and display all items
+            foreach($form_errors as $error){
+                $result .= "<li> {$error} </li>";
+            }
+            $result .= "</ul></p>";
+
+        }else{
+            $result = "<p style='color: red;'> There were " .count($form_errors). " errors in the form: <br>";
+
+            $result .= "<ul style='color: red;'>";
+            //loop through error array and display all items
+            foreach($form_errors as $error){
+                $result .= "<li> {$error}</li>";
+            }
+            $result .= "</ul></p>";
+        }
     }
 
 }
@@ -50,7 +91,7 @@ if (isset($_POST['email'])){
         <tr><td>Email:</td> <td><input type="text" name="email" value=""></td></tr>
         <tr><td>Username:</td> <td><input type="text" name="username" value=""></td></tr>
         <tr><td>Password:</td> <td><input type="password" name="password" value=""></td></tr>
-        <tr><td></td><td><input type="submit" value="Register"></td></tr>
+        <tr><td></td><td><input type="submit" name="btn_register" value="Register"></td></tr>
     </table>
 </form>
 <p><a href="index.php">Back</a> </p>
--- resource/db.php
+++ resource/db.php
@@ -10,7 +10,7 @@ try {
     // set PDO error mode to exception.
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // display success message.
-    echo "Connected to the database.";
+    //echo "Connected to the database.";
 } catch (PDOException $exception){
     // display error message.
     echo "Database connection failed: ".$exception->getMessage();
